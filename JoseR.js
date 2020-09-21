1413bf4b5af90323c0af73ecb8a84ce46e20e624
pythonBoy123 <66473662+pythonBoy123@users.noreply.github.com>
2020-09-21 08:31:27 -0500

    Add files via upload

--- JoseR.js
+++ JoseR.js
@@ -1,78 +1,53 @@
-const discord=require("discord.js");
+const discord=require("discord.js")
 const client=new discord.Client();
 const respuesta=require("./respuesta.json")
+const token="NzU1NDM4ODU2NjEzOTIwNzc4.X2DTQg.Q60vEp1Qz2gFYvXc8UW9YUnpInI";//ingresa el token de tu bot
 
-//http://patorjk.com/software/taag/#p=testall&f=Graffiti&t=rey%0A
 let c={
   c:"",
   calla:0
 }
-
-
-function generateRandom(min, max) {
+function valorAleatorio(min, max) {//obtendra un valor aleatorio
 	min = Math.ceil(min)
 	max = Math.floor(max)
-return Math.floor(Math.random() * (1 + max - min) + min)
+  return Math.floor(Math.random() * (1 + max - min) + min)
 }
+
 client.on("ready",()=>{
-  console.log("no tendra mod")
+  console.log("jose no tendra mod")
   try{
-  c.c=client.channels.cache.get("754113510379880458")
-  }
+  c.c=client.channels.cache.get("754113510379880458")}
   catch(e){
     console.log("no se encontro el canal")
-  }
-  })
-try{
-client.on("message", (message)=>{
-  let m=message.content.split(" ")
-  let nadre=Object.keys(respuesta.palabras).length
-  let naped=respuesta.personas.map((i)=>{
-      if(i==message.author.id){
-      
-      c.calla++;
-      console.log(message.author.username)
-      let mFiltred=m.map((mes)=>{
-        console.log(mes)
-        if(mes=="mod"){
-          console.log("jose no tendra mod")
-          console.log(`mensaje ${m}`)
-          c.c.send(`<@${i}> no tendras mod `);
-          console.log(message.author)
-
-        }
-
-        else if(mes=="admin"){
-          console.log(message.author)
-          console.log("jose no tendra admin")
-          console.log(`mensaje ${m}`)
-          c.c.send(`<@${i}> no tendras admin`);
-
-        }
-
-
-      console.log(c.calla)
-      if(c.calla>generateRandom(5,nadre)){
-        c.calla=0
-        message.channel.send(`<@${i}> calla ${respuesta.palabras[generateRandom(0,nadre)]}`)
-
-
-        //720304994523283497
-      }
-
-      });
-      }
-  })
-    let mmap=m.map((i)=>{
-      if(i.toLowerCase()==":v"){
-        c.c.send(`<@${message.author.id}> calla ${respuesta.palabras[generateRandom(0,nadre)]} `);//mi rey ya fue solucionado
-        //generate random es para escoger alguna palabra o frase aleatoria
-      }
-    })
-
+  };
 })
-}
+  client.on("message", (message)=>{
+    let m=message.content.toLowerCase().split(" ")
+    let tamaño=Object.keys(respuesta.palabras).length-1;
+    let personas=respuesta.personas.map((i)=>{//este es para responderle a los que esten dentro del array personas
+      if(i==message.author.id){
+        c.calla++;
+        let mFiltred=m.map((mes)=>{
+          //respuestas que puede darle en caso que su mensaje contenga alguno de estos valores
+          if(mes=="mod"||mes=="moderador"){
+            c.c.send(`<@${i}> no tendras mod `);
 
-catch(e){
-}
-client.login("token")
+          }
+          if(mes=="admin"||mes=="administrador"){
+            c.c.send(`<@${i}> no tendras admin`)
+          }
+          if(c.calla>valorAleatorio(5,tamaño)){//le responde de manera aleatoria a sus mensajes
+            c.calla=0;
+            message.channel.send(`<@${i}> calla ${respuesta.palabras[valorAleatorio(0,tamaño)]}`);
+          }
+      });
+    }
+  })
+  let mmap=m.map((i)=>{
+    if(i==":v"||i==":u"||i==";v"||i==";v"){//este es para responderle a los papulinces
+      c.c.send(`<@${message.author.id}> calla ${respuesta.palabras[valorAleatorio(0,tamaño)]} `);//mi rey ya fue solucionado
+        //valor aleatorio lo uso para obtener alguna respuesta aleatoria del json
+    }
+  })
+})
+client.login(token)
